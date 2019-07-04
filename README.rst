0b904d9248cd49f7bf63053b75c5112454a9d24f
Aleksey Denysyuk <diplomt@gmailcom>
2019-07-04 14:52:08 +0200

    Basic package structure with tests

---
+++ README.rst
@@ -0,0 +1,8 @@
+JIRA ORM
+--------
+
+Wrapper around JIRA uses ORM approach along with Container pattern to create local representation of remote JIRA data.
+
+Current limitations planned to be improved:
+    - no support of persistency
+    - multithreading
---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(
+    name='jiraorm',
+    version='0.1',
+    description='Object oriented wrapper around JIRA.',
+    long_description=readme(),
+    url='TBD',
+    author='Aleksey Denysyuk',
+    author_email='diplomt@gmail.com',
+    license='MIT', #TBD
+    packages=['jiraorm'],
+    install_requires=['jira'],
+    #dependency_links=['http://server/user/repo/tarball/master#egg=package-1.0']
+    include_package_data=True,
+    test_suite='nose.collector',
+    tests_require=['nose'],
+    zip_safe=False
+)
